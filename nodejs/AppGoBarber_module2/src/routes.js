0538e19e533a5e22ac4ded1e0da945b715da473c
Alex Ford Botelho <alex.botelho@gruposolutions.com.br>
2019-04-23 15:18:48 -0300

    App GoBarber initialization

---
+++ nodejs/AppGoBarber_module2/src/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const UserController = require("./app/controllers/UserController");
+
+const routes = express.Router();
+
+routes.get("/signup", UserController.create);
+routes.post("/signup", UserController.store);
+
+module.exports = routes;
