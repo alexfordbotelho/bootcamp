3dbdb6887e42cca4148ff271c33d3316a1085e0d
Alex Ford Botelho <alex.botelho@gruposolutions.com.br>
2019-04-23 17:24:59 -0300

    App GoBarber - implementing insert new user

--- nodejs/AppGoBarber_module2/src/routes.js
+++ nodejs/AppGoBarber_module2/src/routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const multerconfig = require("./config/multer");
+const upload = require("multer")(multerconfig);
 
 const UserController = require("./app/controllers/UserController");
 
 const routes = express.Router();
 
 routes.get("/signup", UserController.create);
-routes.post("/signup", UserController.store);
+routes.post("/signup", upload.single("avatar"), UserController.store);
 
 module.exports = routes;
