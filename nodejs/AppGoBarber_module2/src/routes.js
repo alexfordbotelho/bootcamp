0618fb28715f1fb1888c7af86372c5eee021d3f1
Alex Ford Botelho <alex.botelho@gruposolutions.com.br>
2019-04-25 13:41:15 -0300

    Ajust schedule

--- nodejs/AppGoBarber_module2/src/routes.js
+++ nodejs/AppGoBarber_module2/src/routes.js
@@ -4,10 +4,13 @@ const upload = require("multer")(multerconfig);
 
 const UserController = require("./app/controllers/UserController");
 const SessionController = require("./app/controllers/SessionController");
+const FileController = require("./app/controllers/FileController");
 const authMiddleware = require("./app/middleware/auth");
 const guestMiddleware = require("./app/middleware/guest");
 
 const DashboardController = require("./app/controllers/DashboardController");
+const AppointmentsController = require("./app/controllers/AppointmentsController");
+const AvailableController = require("./app/controllers/AvailableController");
 
 const routes = express.Router();
 
@@ -18,6 +21,8 @@ routes.use((req, res, next) => {
   return next();
 });
 
+routes.get("/files/:file", FileController.show);
+
 routes.get("/", guestMiddleware, SessionController.create);
 routes.post("/signin", SessionController.store);
 
@@ -27,6 +32,9 @@ routes.post("/signup", upload.single("avatar"), UserController.store);
 routes.use("/app", authMiddleware);
 
 routes.get("/app/dashboard", DashboardController.index);
+routes.get("/app/appointments/new/:provider", AppointmentsController.create);
+routes.post("/app/appointments/new/:provider", AppointmentsController.store);
+routes.get("/app/available/:provider", AvailableController.index);
 
 routes.get("/app/logout", SessionController.destroy);
 
